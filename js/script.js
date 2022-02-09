b3cc4fb14574a7a1eae94be578a8eeffc7f0463d
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-02-09 16:00:54 -0300

    adicionando id em cada elemento de mensagem

--- js/script.js
+++ js/script.js
@@ -8,27 +8,28 @@ function getMensagens(){
 function renderizarMensagem(mensagem){    
     msg = mensagem.data; 
     console.log(msg);   
-    let conteudo = document.querySelector(".conteudo");    
+    let conteudo = document.querySelector(".conteudo");      
     for(let i = 0; i < msg.length; i++){
         if(msg[i].text === 'entra na sala...' || msg[i].text === 'sai da sala...'){
             conteudo.innerHTML += `    
-                <div class="div-mensagens">
+                <div class="div-mensagens" id=${[i]}>
                 <div class="time"><time>(${msg[i].time})</time></div>  
                     <strong>${msg[i].from}</strong>                                
                     ${msg[i].text}
                 </div>        
             `;
+            
             }else{
                 conteudo.innerHTML += `    
-                    <div class="div-entrada-saida">
+                    <div class="div-entrada-saida" id=${[i]}>
                         <div class="time"><time>(${msg[i].time})</time></div>                        
-                        <strong>${msg[i].from}</strong>
-                        <span>para</span>            
+                        <strong>${msg[i].from}</strong><span>para</span>
+                                    
                         <strong>${msg[i].to}</strong>
                         ${msg[i].text}
                     </div>        
                 `;
-            }        
-        }   
+        }         
+    }     
 }
 
